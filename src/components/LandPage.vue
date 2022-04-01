2130972a3663edfd95aff8bc2155f37c3087048d
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-04-01 08:26:22 +0100

    created Land component and view

---
+++ src/components/LandPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="land"></div>
+</template>
+
+<script>
+export default {
+  name: "LandPage",
+  props: {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/LandView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <LandPage></LandPage>
+  </div>
+</template>
+
+<script>
+import LandPage from "@/components/LandPage.vue";
+
+export default {
+  name: "LandView",
+  components: {
+    LandPage,
+  },
+};
+</script>
