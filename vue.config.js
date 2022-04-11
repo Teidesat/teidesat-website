6dcd2f2e6b55faa4766070f543a02ccede9daaa5
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-04-11 09:22:51 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/teidesat_web/",
 });
 
-// module.exports = {
-//   publicPath:
-//     ProcessingInstruction.env.NODE_ENV === "production" ? "teidesat_web" : "/",
-// };
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "teidesat_web" : "/",
+};
