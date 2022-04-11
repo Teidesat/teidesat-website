c3ee2cb5980b0638f894d0905e77584d30f3dcea
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-04-11 20:28:24 +0100

    changed freq questions

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@
 // });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/teidesat-web/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/teidesat-website/" : "/",
 };
