80c4878c4a6f5c03c18c96f9fe12c98472a9e721
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-04-11 09:15:54 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:FabioBianchiniCano/FabioBianchiniCano.github.io  master
+# git push -f git@github.com:FabioBianchiniCano/FabioBianchiniCano.github.io  master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:FabioBianchiniCano/teidesat-web.git main:gh-pages
 
 cd -
