32837254f9452e985ae8aa2030cd81969cf93725
Guillermo García Hernández <guigarher@gmail.com>
2026-03-18 13:21:38 +0000

    Nueva landing page TEIDESAT y reorganización de templates en views/pages

--- addons/teidesat_website/__manifest__.py
+++ addons/teidesat_website/__manifest__.py
@@ -5,12 +5,19 @@
     'category': 'Website',
     'depends': ['website'],
     'data': [
-        'views/templates.xml',
+    'views/templates.xml',
+    'views/pages/home.xml',
+    'views/pages/divulgacion.xml',
+    'views/pages/nosotros.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'teidesat_website/static/src/css/style.css',
             'teidesat_website/static/src/js/script.js',
+            'teidesat_website/static/src/css/home.css',
+            'teidesat_website/static/src/css/divulgacion.css',
+            'teidesat_website/static/src/css/nosotros.css',
+            'teidesat_website/static/src/js/home.js',
         ],
     },
     'installable': True,
--- addons/teidesat_website/controllers/main.py
+++ addons/teidesat_website/controllers/main.py
@@ -4,6 +4,14 @@ from odoo.http import request
 
 class TeidesatWebsite(http.Controller):
 
-    @http.route('/teidesat', type='http', auth='public', website=True)
-    def teidesat_page(self, **kwargs):
+    @http.route('/', type='http', auth='public', website=True)
+    def teidesat_home(self, **kwargs):
         return request.render('teidesat_website.teidesat_homepage')
+
+    @http.route('/divulgacion', type='http', auth='public', website=True)
+    def teidesat_divulgacion(self, **kwargs):
+        return request.render('teidesat_website.page_divulgacion')
+
+    @http.route('/nosotros', type='http', auth='public', website=True)
+    def teidesat_nosotros(self, **kwargs):
+        return request.render('teidesat_website.page_nosotros')
