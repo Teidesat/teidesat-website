d813abfe33019c9fef8a75841b6a03065bcb7cb3
Guillermo García Hernández <guigarher@gmail.com>
2026-03-18 14:34:51 +0000

    mejoras css del home

--- addons/teidesat_website/__manifest__.py
+++ addons/teidesat_website/__manifest__.py
@@ -5,15 +5,12 @@
     'category': 'Website',
     'depends': ['website'],
     'data': [
-    'views/templates.xml',
-    'views/pages/home.xml',
-    'views/pages/divulgacion.xml',
-    'views/pages/nosotros.xml',
+        'views/pages/home.xml',
+        'views/pages/divulgacion.xml',
+        'views/pages/nosotros.xml',
     ],
     'assets': {
         'web.assets_frontend': [
-            'teidesat_website/static/src/css/style.css',
-            'teidesat_website/static/src/js/script.js',
             'teidesat_website/static/src/css/home.css',
             'teidesat_website/static/src/css/divulgacion.css',
             'teidesat_website/static/src/css/nosotros.css',
