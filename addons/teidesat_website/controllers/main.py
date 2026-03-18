dfc58d2cc07a34214e3d4fd3e3d51fece5c4e3fb
Guillermo García Hernández <guigarher@gmail.com>
2026-03-18 11:55:27 +0000

    Revert estructura inicial módulo teidesat website

--- addons/teidesat_website/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-
-class TeidesatWebsite(http.Controller):
-
-    @http.route('/teidesat', type='http', auth='public', website=True)
-    def teidesat_page(self, **kwargs):
-        return request.render('teidesat_website.teidesat_homepage')
