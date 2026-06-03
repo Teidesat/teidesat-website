2abce9d08e162245654a7892dedd986663e52724
Rodrigo <ro19dri06go@gmail.com>
2026-06-03 11:15:16 +0100

    Aleatoriedad de las respuestas del teidesatkid

--- addons/teidesat_website/static/src/js/teidesatkids_game.js
+++ addons/teidesat_website/static/src/js/teidesatkids_game.js
@@ -177,8 +177,8 @@ function initTeidesatKidsGame() {
         },
         {
             question: "¿Qué planeta es nuestro hogar?",
-            options: ["Marte", "La Tierra", "Júpiter"],
-            correct: 1
+            options: ["La Tierra", "Marte", "Júpiter"],
+            correct: 0
         },
         {
             question: "¿Para qué sirve una antena en un satélite?",
@@ -202,8 +202,8 @@ function initTeidesatKidsGame() {
         },
         {
             question: "¿Qué necesita un satélite para comunicarse?",
-            options: ["Una antena", "Un paracaídas", "Una rueda"],
-            correct: 0
+            options: ["Un paracaídas", "Una antena", "Una rueda"],
+            correct: 1
         },
         {
             question: "¿Qué protege a la Tierra de muchos meteoritos pequeños?",
@@ -329,6 +329,13 @@ function initTeidesatKidsGame() {
         playerVisible = false;
     }
 
+    function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    }
+
     function showRescueQuestion(cause = "obstacle") {
         if (rescueUsed) {
             triggerGameOverExplosion();
@@ -338,8 +345,19 @@ function initTeidesatKidsGame() {
         rescueUsed = true;
         rescueCause = cause;
         waitingForAnswer = true;
-        pendingQuestion = scienceQuestions[Math.floor(Math.random() * scienceQuestions.length)];
 
+        pendingQuestion = structuredClone(
+        scienceQuestions[Math.floor(Math.random() * scienceQuestions.length)]
+        );
+
+
+        const correctAnswer = pendingQuestion.options[pendingQuestion.correct];
+
+        shuffleArray(pendingQuestion.options);
+        
+
+        pendingQuestion.correct = pendingQuestion.options.indexOf(correctAnswer);
+        
         keys.up = false;
         keys.down = false;
         keys.left = false;
